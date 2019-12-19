1752f980ea33f29eed1a6b09656a11cf31db84fa
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-18 22:22:40 -0300

    parte 1

---
+++ src/Pessoa/Pessoa.js
@@ -0,0 +1,31 @@
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Pessoa.css'
+import Auxiliar from '../hoc/Auxiliar';
+import comClasse from '../hoc/comClasse'
+
+class Pessoa extends Component {
+
+   render() {
+      console.log('[Pessoa.js] rendering...') 
+      return (
+         <Auxiliar>
+            <p key="i1" onClick={this.props.click}>Eu sou {this.props.nome}, Eu sou uma Pessoa e tenho {this.props.idade} anos !</p>
+            <p key="i2" >{this.props.children}</p>
+            <input key="i3" type='text' onChange={this.props.changed} value={this.props.nome}></input>
+         </Auxiliar>
+      );
+   }
+   
+}
+
+Pessoa.propTypes = {
+   click: PropTypes.func,
+   nome: PropTypes.string,
+   idade: PropTypes.number,
+   changed: PropTypes.func
+};
+
+export default comClasse(Pessoa, classes.Pessoa);
